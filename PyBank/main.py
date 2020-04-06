f40abbb18f51cc8e770d6cb2bbde66fdbfeb0d22
iris28kurti <iris28kurti@gmail.com>
2020-04-06 15:44:49 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-
+# Dependencies and Set variables
 
 import os
 import csv
@@ -6,29 +6,35 @@ total_net_list = []
 change =[]
 i=0 
 
+# Set path for file to read and save the output file 
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 output_file = os.path.join("..", "Analysis","pybank.txt")
 
+
+# Read the csv and convert it into a list of dictionaries
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+
     for row in csvreader:
+        # Track the total
         total_net_list.append(int(row[1]))
         months_count = len(total_net_list)
         total_net=sum(total_net_list)
         
     for i in range(len(total_net_list)-1):
+        # Track the average change
             change.append(int(total_net_list[i+1]) - int(total_net_list[i]))
             average_change=sum(change) /len(change)
             average_change=round(average_change,2)
       
-        
+        # Calculate the greatest increase and the greatest decrease
             greatest_increase=int(max(change))
             greatest_decrease=int(min(change))
 
     
     
-
+# Print the output (to terminal)
     print("Financial Analysis")
     print("-----------------------------------------------")
     print("Total Months:  " + str(months_count))
@@ -40,7 +46,7 @@ with open(csvpath, newline='') as csvfile:
     print(f'Greatest Decrease in Profits: ${greatest_decrease}')
     print("-----------------------------------------------")
     
-    
+# Export the results to text file 
 with open(output_file,"w", newline='') as textfile:
     writer=csv.writer(textfile)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,39 +1,46 @@
-
+# Dependencies
 import os
 import csv
 
+# Set variables
 total_votes=0
 winning_count=0
 candidate_option = []
 candidate_votes = {}
 
+# Files to load and output
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 output_file = os.path.join("..", "Analysis","election_data_output.txt")
 
 
-
+# Read the csv and convert it into a list of dictionaries
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+
+    # Extract the candidate name from each row
     for row in csvreader:
         candidate_list=row[2]
         total_votes=total_votes+1
+        #  If the candidate does not match any existing candidate then add it to the list of candidates in the running
         if candidate_list not in candidate_option:
             candidate_option.append(candidate_list) 
             candidate_votes[candidate_list]=0
         candidate_votes[candidate_list]=candidate_votes[candidate_list]+1
-       
+
+  # Retrieve vote count and percentage      
 for candidate in candidate_votes:
     votes=candidate_votes.get(candidate)
     vote_percent=float(votes/total_votes)*100
     vote_percent=round(vote_percent,2)
-          
+
+     # Determine winning vote count and candidate      
     if (votes>winning_count):
         winning_count=votes
         winning_candidate=candidate
         
 
-
+# Print the output (to terminal)
 print("\nElection Results\n")
 print("-----------------------------------\n")
 print(f'Total Votes: {total_votes}\n')
@@ -48,6 +55,8 @@ print("\n-----------------------------------\n")
 print(f'Winner: {winning_candidate}\n')
 print("-----------------------------------")
 
+
+# Print the results and export the data to our text file
 with open(output_file,"w", newline='') as txt_file:
     writer=csv.writer(txt_file)
 
